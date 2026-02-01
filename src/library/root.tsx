96ddfb03e910890af706e99c212d4e9c971917fb
fachrigans <fachri@gmail.com>
2026-02-01 22:04:01 +0700

    feat: navbar

--- src/library/root.tsx
+++ src/library/root.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../component/navbar";
 
 export default function Root() {
   return (
     <>
+    <Navbar/>
       <div>
         <Outlet />
       </div>
