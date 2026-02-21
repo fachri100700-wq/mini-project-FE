c7d821fdf1e3561457089aa3e47eb853495dda1f
fachrigans <fachri@gmail.com>
2026-02-22 00:31:53 +0700

    fix router

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from './library/root';
+import ExploreEvent from './app/exploreEvent/exploreEvent';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "/explore-event", element: <ExploreEvent /> },
+    ],
   },
 ]);
 
