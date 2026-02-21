fe00fc55e0648079c99df4eb4ef0c133ad0f1782
fachrigans <fachri@gmail.com>
2026-02-22 00:18:49 +0700

    Create feat admin vertification

---
+++ src/utils/axios-instance.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_PUBLIC_API_URL,
+  withCredentials: true,
+});
+
+
+console.log(import.meta.env.VITE_PUBLIC_API_URL);
+
+export default axiosInstance;
