7b5ed43f2be0dfaf946d8a29992b8839dbc63daa
fachrigans <fachri@gmail.com>
2026-02-23 09:48:49 +0700

    adjust margin top

--- src/app/exploreEvent/exploreEvent.tsx
+++ src/app/exploreEvent/exploreEvent.tsx
@@ -80,7 +80,7 @@ export default function ExploreEvent() {
   };
 
   return (
-    <div className="min-h-screen bg-gray-50 pt-10 pb-24 px-4 md:px-10">
+    <div className="min-h-screen mt-10 bg-gray-50 pt-10 pb-24 px-4 md:px-10">
       <div className="max-w-7xl flex gap-8">
         {/* SIDEBAR */}
         <SideBarFilter
